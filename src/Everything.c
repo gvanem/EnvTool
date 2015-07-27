3cc5e9ef94519128ecffe8e52bbe0b35f8198bf3
Gisle Vanem <gvanem@yahoo.no>
2015-07-27 23:51:09 +0000

    Add wcsicmp() for CygWin. :tophat:

--- src/Everything.c
+++ src/Everything.c
@@ -21,13 +21,17 @@
 #include "Everything.h"
 #include "Everything_IPC.h"
 
-#if defined(__CYGWIN__) && 0
-static int wcsicmp ( const wchar_t *s1 __attribute__((notused)),
-                     const wchar_t *s2 __attribute__((notused)) )
-{
-  fprintf (stderr, "CygWin doesn't have 'wcsicmp()'. Exiting\n");
-  exit(1);
-}
+#if defined(__CYGWIN__)
+  #define stricmp(s1, s2)  strcasecmp (s1, s2)
+
+  #if 1
+    static int wcsicmp ( const wchar_t *s1 __attribute__((unused)),
+                         const wchar_t *s2 __attribute__((unused)) )
+    {
+      fprintf (stderr, "CygWin doesn't have 'wcsicmp()'. Exiting\n");
+      exit(1);
+    }
+  #endif
 #endif
 
 /*
