49e80190c6bb612b4ff139d62a4615d221ab3e54
gvanem <gvanem@yahoo.no>
2017-07-24 08:52:36 +0000

    Moved typedef to the top. The 'smartlist_read_func()' parse-function takes a 'const char *line'.

--- src/smartlist.h
+++ src/smartlist.h
@@ -1,13 +1,15 @@
 #ifndef _SMARTLIST_H
 #define _SMARTLIST_H
 
-#include <stdlib.h>
-
 #if defined(INSIDE_SMARTLIST_C)
   typedef struct smartlist_t smartlist_t;        /* Forward */
 #else
   typedef struct smartlist_internal smartlist_t; /* Opaque struct */
 #endif
+
+typedef int  (__cdecl *smartlist_compare_func) (const void *key, const void **member);
+typedef void (__cdecl *smartlist_parse_func) (smartlist_t *sl, const char *line);
+
 
 int          smartlist_len (const smartlist_t *sl);
 void        *smartlist_get (const smartlist_t *sl, int idx);
@@ -19,8 +21,6 @@ void  smartlist_free_all (smartlist_t *sl);
 void  smartlist_ensure_capacity (smartlist_t *sl, size_t num);
 void  smartlist_add (smartlist_t *sl, void *element);
 
-typedef int (__cdecl *smartlist_compare_func) (const void *key, const void **member);
-
 void  smartlist_sort (smartlist_t *sl, smartlist_compare_func compare);
 
 int   smartlist_bsearch_idx (const smartlist_t *sl, const void *key,
@@ -28,9 +28,6 @@ int   smartlist_bsearch_idx (const smartlist_t *sl, const void *key,
 
 void *smartlist_bsearch (const smartlist_t *sl, const void *key,
                          smartlist_compare_func compare);
-
-typedef void (__cdecl *smartlist_parse_func) (smartlist_t *sl,
-                                              const void *line);
 
 smartlist_t *smartlist_read_file (const char *file,
                                   smartlist_parse_func parse);
