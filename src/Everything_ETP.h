5254591f271006e8fefb4c10ed517acc562ce1b6
gvanem <gvanem@yahoo.no>
2017-08-01 15:10:13 +0000

    Fix prototype for 'do_check_evry_ept()'.

--- src/Everything_ETP.h
+++ src/Everything_ETP.h
@@ -1,7 +1,7 @@
 #ifndef _EVERYTHING_ETP_H_
 #define _EVERYTHING_ETP_H_
 
-extern int  do_check_evry_ept (void);
+extern int  do_check_evry_ept (const char *host);
 extern int  netrc_init (void);
 extern void netrc_exit (void);
 extern int  netrc_lookup (const char *host, const char **user, const char **passw);
