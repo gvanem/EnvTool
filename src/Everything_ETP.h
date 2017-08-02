ba57abe92bf71ca849491f2b2c5cdb5ead1732ec
gvanem <gvanem@yahoo.no>
2017-08-02 16:43:27 +0000

    Added total-receive counter 'ETP_total_rcv'.

--- src/Everything_ETP.h
+++ src/Everything_ETP.h
@@ -1,6 +1,8 @@
 #ifndef _EVERYTHING_ETP_H_
 #define _EVERYTHING_ETP_H_
 
+extern DWORD ETP_total_rcv;
+
 extern int  do_check_evry_ept (const char *host);
 extern int  netrc_init (void);
 extern void netrc_exit (void);
