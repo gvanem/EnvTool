641899b640df915de27e2f322d9b7ebc20b23295
gvanem <gvanem@yahoo.no>
2017-09-28 10:40:12 +0000

    Rewritten using a set of 'common_*()' functions. There is now only one  smartlist of 'struct login_info*' for both '.netrc' and '.authinfo' records.

--- src/auth.c
+++ src/auth.c
@@ -14,38 +14,125 @@
 #include "smartlist.h"
 #include "auth.h"
 
-struct netrc_info {
+struct login_info {
        BOOL  is_default;
-       char *host;
-       char *user;
-       char *passw;
-     };
-
-struct auth_info {
-       BOOL  is_default;
+       BOOL  is_netrc;
        char *host;
        char *user;
        char *passw;
        int   port;
      };
 
-static smartlist_t *netrc, *authinfo;
+static smartlist_t *login_list = NULL;
 
 /*
  * Common to both 'netrc_init()' and 'authinfo_init()'.
+ * The 'login_list' is grown in the 'parser' functions below.
  */
-static smartlist_t *common_init (const char *fname, smartlist_parse_func parser)
+static int common_init (const char *fname, smartlist_parse_func parser)
 {
+  char        *file = getenv_expand (fname);
   smartlist_t *sl;
-  char *file = getenv_expand (fname);
+  BOOL         first_time = (login_list == NULL);
 
   sl = file ? smartlist_read_file (file, parser) : NULL;
-  if (!sl)
-     WARN ("Failed to open \"%s\". Authenticated logins will not work.\n", fname);
   FREE (file);
-  return (sl);
+
+  if (!sl)
+  {
+    WARN ("Failed to open \"%s\". Authenticated logins will not work.\n", fname);
+    return (0);
+  }
+
+  if (first_time)
+     login_list = sl;
+  else
+  {
+    smartlist_append (login_list, sl);
+    smartlist_free (sl);
+  }
+  return (1);
 }
 
+/*
+ * Free the memory allocated in the 'login_list' smartlist.
+ */
+static void common_exit (BOOL is_netrc)
+{
+  int i, max;
+
+  if (!login_list)
+     return;
+
+  max = smartlist_len (login_list);
+  DEBUGF (2, "is_netrc: %d, length of list now: %d\n", is_netrc, max);
+
+  for (i = 0; i < max; i++)
+  {
+    struct login_info *li = smartlist_get (login_list, i);
+
+    DEBUGF (2, "i: %2d, li->is_netrc: %d, do %sdelete this.\n",
+            i, li->is_netrc, (li->is_netrc == is_netrc) ? "" : "not ");
+
+    if (li->is_netrc != is_netrc)
+       continue;
+
+    FREE (li->host);
+    FREE (li->user);
+    FREE (li->passw);
+    FREE (li);
+    smartlist_del (login_list, i);
+    max--;
+    i--;
+  }
+
+  DEBUGF (2, "length of list now: %d\n", smartlist_len(login_list));
+
+  if (smartlist_len(login_list) == 0)
+  {
+    smartlist_free (login_list);
+    login_list = NULL;
+  }
+}
+
+/*
+ * Search the 'login_list' smartlist for 'host'.
+ */
+static const struct login_info *common_lookup (const char *host, BOOL is_netrc)
+{
+  const struct login_info *def_li = NULL;
+  int   i, max = login_list ? smartlist_len (login_list) : 0;
+
+  for (i = 0; i < max; i++)
+  {
+    const struct login_info *li = smartlist_get (login_list, i);
+    char  buf [300];
+
+    if (li->is_netrc != is_netrc)
+       continue;
+
+    if (li->is_default)
+       def_li = li;
+
+    snprintf (buf, sizeof(buf), "is_netrc: %d, host: '%s', user: '%s', passw: '%s', port: %d\n",
+              li->is_netrc, li->is_default ? "*default*" : li->host, li->user, li->passw, li->port);
+
+    if (opt.do_tests)
+    {
+      C_setraw (1);
+      C_printf ("  %s", buf);
+      C_setraw (0);
+    }
+    else
+      DEBUGF (3, buf);
+
+    if (li->host && host && !stricmp(host, li->host))
+       return (li);
+  }
+  if (def_li)
+     return (def_li);
+  return (NULL);
+}
 
 /*
  * Parse a line from '~/.netrc'. Match lines like:
@@ -53,11 +140,11 @@ static smartlist_t *common_init (const char *fname, smartlist_parse_func parser)
  * Or
  *   default login <user> password <password>
  *
- * And add to the given smartlist.
+ * And add to the 'login_list' smartlist.
  */
 static void netrc_parse (smartlist_t *sl, const char *line)
 {
-  struct netrc_info *ni;
+  struct login_info *li = NULL;
   char   host [256];
   char   user [50];
   char   passw[50];
@@ -66,90 +153,82 @@ static void netrc_parse (smartlist_t *sl, const char *line)
 
   if (sscanf(line, fmt1, host, user, passw) == 3)
   {
-    ni = CALLOC (1, sizeof(*ni));
-    ni->host  = STRDUP (host);
-    ni->user  = STRDUP (user);
-    ni->passw = STRDUP (passw);
-    smartlist_add (sl, ni);
+    li = CALLOC (1, sizeof(*li));
+    li->host     = STRDUP (host);
+    li->user     = STRDUP (user);
+    li->passw    = STRDUP (passw);
+    li->is_netrc = TRUE;
   }
   else if (sscanf(line, fmt2, user, passw) == 2)
   {
-    ni = CALLOC (1, sizeof(*ni));
-    ni->user       = STRDUP (user);
-    ni->passw      = STRDUP (passw);
-    ni->is_default = TRUE;
-    smartlist_add (sl, ni);
+    li = CALLOC (1, sizeof(*li));
+    li->user       = STRDUP (user);
+    li->passw      = STRDUP (passw);
+    li->is_default = TRUE;
+    li->is_netrc   = TRUE;
   }
+  if (li)
+     smartlist_add (sl, li);
+}
+
+/*
+ * Parse a line from '~/.authinfo'. Match lines like:
+ *   machine <host> port <num> login <user> password <password>
+ * Or
+ *   default port <num> login <user> password <password>
+ *
+ * And add to the 'login_list' smartlist.
+ */
+static void authinfo_parse (smartlist_t *sl, const char *line)
+{
+  struct login_info *li = NULL;
+  char   host [256];
+  char   user [50];
+  char   passw[50];
+  int    port = 0;
+  const char *fmt1 = "machine %256s port %d login %50s password %50s";
+  const char *fmt2 = "default port %d login %50s password %50s";
+
+  if (sscanf(line, fmt1, host, &port, user, passw) == 4 && port > 0 && port < USHRT_MAX)
+  {
+    li = CALLOC (1, sizeof(*li));
+    li->host     = STRDUP (host);
+    li->user     = STRDUP (user);
+    li->passw    = STRDUP (passw);
+    li->port     = port;
+    li->is_netrc = FALSE;
+  }
+  else if (sscanf(line, fmt2, &port, user, passw) == 3 && port > 0 && port < USHRT_MAX)
+  {
+    li = CALLOC (1, sizeof(*li));
+    li->user       = STRDUP (user);
+    li->passw      = STRDUP (passw);
+    li->port       = port;
+    li->is_default = TRUE;
+    li->is_netrc   = FALSE;
+  }
+  if (li)
+     smartlist_add (sl, li);
 }
 
 int netrc_init (void)
 {
-  netrc = common_init ("%APPDATA%\\.netrc", netrc_parse);
-  return (netrc != NULL);
+  return common_init ("%APPDATA%\\.netrc", netrc_parse);
 }
 
-/*
- * Free the memory allocated in the 'netrc' smartlist.
- */
+int authinfo_init (void)
+{
+  return common_init ("%APPDATA%\\.authinfo", authinfo_parse);
+}
+
 void netrc_exit (void)
 {
-  int i, max;
-
-  if (!netrc)
-     return;
-
-  max = smartlist_len (netrc);
-  for (i = 0; i < max; i++)
-  {
-    struct netrc_info *ni = smartlist_get (netrc, i);
-
-    FREE (ni->host);
-    FREE (ni->user);
-    FREE (ni->passw);
-    FREE (ni);
-  }
-  smartlist_free (netrc);
-  netrc = NULL;
+  common_exit (TRUE);
 }
 
-/*
- * Lookup the user/password for a 'host' in the 'netrc' smartlist.
- */
-static const struct netrc_info *_netrc_lookup (const char *host)
+void authinfo_exit (void)
 {
-  const struct netrc_info *def_ni = NULL;
-  int   i, max;
-
-  if (!netrc)
-     return (NULL);
-
-  max = smartlist_len (netrc);
-  for (i = 0; i < max; i++)
-  {
-    const struct netrc_info *ni = smartlist_get (netrc, i);
-    char  buf [300];
-
-    if (ni->is_default)
-       def_ni = ni;
-
-    snprintf (buf, sizeof(buf), "host: '%s', user: '%s', passw: '%s', is_default: %d.\n",
-              ni->host, ni->user, ni->passw, ni->is_default);
-
-    if (opt.do_tests)
-    {
-      C_setraw (1);
-      C_printf ("  %s", buf);
-      C_setraw (0);
-    }
-    else
-      DEBUGF (3, buf);
-
-    if (ni->host && host && !stricmp(host, ni->host))
-       return (ni);
-  }
-  if (def_ni)
-     return (def_ni);
-  return (NULL);
+  common_exit (FALSE);
 }
 
 /*
@@ -158,122 +237,16 @@ static const struct netrc_info *_netrc_lookup (const char *host)
  */
 int netrc_lookup (const char *host, const char **user, const char **passw)
 {
-  const struct netrc_info *ni = _netrc_lookup (host);
+  const struct login_info *li = common_lookup (host, TRUE);
 
-  if (ni)
-  {
-    if (user)
-       *user  = ni->user;
-    if (passw)
-       *passw = ni->passw;
-  }
-  return (ni != NULL);
-}
+  if (!li)
+     return (0);
 
-/*
- * Parse a line from '~/.authinfo'. Match lines like:
- *   machine <host> port <num> login <user> password <password>
- *
- * And add to the given smartlist.
- */
-static void authinfo_parse (smartlist_t *sl, const char *line)
-{
-  struct auth_info *ai;
-  char   host [256];
-  char   user [50];
-  char   passw[50];
-  int    port;
-  const char *fmt1 = "machine %256s port %d login %50s password %50s";
-  const char *fmt2 = "default port %d login %50s password %50s";
-
-  if (sscanf(line, fmt1, host, &port, user, passw) == 4 && port > 0 && port < USHRT_MAX)
-  {
-    ai = CALLOC (1, sizeof(*ai));
-    ai->host  = STRDUP (host);
-    ai->user  = STRDUP (user);
-    ai->passw = STRDUP (passw);
-    ai->port  = port;
-    smartlist_add (sl, ai);
-  }
-  else if (sscanf(line, fmt2, &port, user, passw) == 3)
-  {
-    ai = CALLOC (1, sizeof(*ai));
-    ai->user       = STRDUP (user);
-    ai->passw      = STRDUP (passw);
-    ai->port       = port;
-    ai->is_default = TRUE;
-    smartlist_add (sl, ai);
-  }
-}
-
-int authinfo_init (void)
-{
-  authinfo = common_init ("%APPDATA%\\.authinfo", authinfo_parse);
-  return (authinfo != NULL);
-}
-
-/*
- * Free the memory allocated in the 'authinfo' smartlist.
- */
-void authinfo_exit (void)
-{
-  int i, max;
-
-  if (!authinfo)
-     return;
-
-  max = smartlist_len (authinfo);
-  for (i = 0; i < max; i++)
-  {
-    struct auth_info *ai = smartlist_get (authinfo, i);
-
-    FREE (ai->host);
-    FREE (ai->user);
-    FREE (ai->passw);
-    FREE (ai);
-  }
-  smartlist_free (authinfo);
-  authinfo = NULL;
-}
-
-/*
- * Lookup the user/password/port for a 'host' in the 'authinfo' smartlist.
- */
-static const struct auth_info *_authinfo_lookup (const char *host)
-{
-  const struct auth_info *def_ai = NULL;
-  int   i, max;
-
-  if (!authinfo)
-     return (NULL);
-
-  max = smartlist_len (authinfo);
-  for (i = 0; i < max; i++)
-  {
-    const struct auth_info *ai = smartlist_get (authinfo, i);
-    char  buf [300];
-
-    if (ai->is_default)
-       def_ai = ai;
-
-    snprintf (buf, sizeof(buf), "host: '%s', user: '%s', passw: '%s', port: %d\n",
-              ai->host, ai->user, ai->passw, ai->port);
-
-    if (opt.do_tests)
-    {
-      C_setraw (1);
-      C_printf ("  %s", buf);
-      C_setraw (0);
-    }
-    else
-      DEBUGF (3, buf);
-
-    if (ai->host && host && !stricmp(host, ai->host))
-       return (ai);
-  }
-  if (def_ai)
-     return (def_ai);
-  return (NULL);
+  if (user)
+     *user = li->user;
+  if (passw)
+     *passw = li->passw;
+  return (li != NULL);
 }
 
 /*
@@ -282,17 +255,17 @@ static const struct auth_info *_authinfo_lookup (const char *host)
  */
 int authinfo_lookup (const char *host, const char **user, const char **passw, int *port)
 {
-  const struct auth_info *ai = _authinfo_lookup (host);
+  const struct login_info *li = common_lookup (host, FALSE);
 
-  if (ai)
-  {
-    if (user)
-       *user  = ai->user;
-    if (passw)
-       *passw = ai->passw;
-    if (port)
-       *port = ai->port;
-  }
-  return (ai != NULL);
+  if (!li)
+     return (0);
+
+  if (user)
+     *user = li->user;
+  if (passw)
+     *passw = li->passw;
+  if (port)
+     *port = li->port;
+  return (1);
 }
 
