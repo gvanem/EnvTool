531befcd0a962d48e038f2c4c851049d6c2c0a30
gvanem <gvanem@yahoo.no>
2017-11-19 10:31:23 +0000

    *) New functions:     'cfg_ignore_first()': returns 1st ignore-value in a section.     'cfg_ignore_next()':  returns the next in same section.     'cfg_ignore_dump()':  dump all ignorables in all sections.
    *) 'cfg_ignore_lookup()' must now be called with section-name
       as "[Section]".
    
    *) Use 'list_lookup_value()' to find a valid section in 'cfg_parse()'.

--- src/ignore.c
+++ src/ignore.c
@@ -2,17 +2,20 @@
  * \file    ignore.c
  * \ingroup Misc
  * \brief
- *   Support for reading a config-file with things to ignore.
+ *   Support for reading a config-file with things to
+ *   ignore at run-time.
  */
-
 #include "envtool.h"
 #include "color.h"
 #include "smartlist.h"
 #include "ignore.h"
 
-static const char *sections[] = {
-                  "Registry", "Python", "PE-resources"
-                };
+const struct search_list sections[] = {
+                       { 0, "[Compiler]" },
+                       { 1, "[Registry]" },
+                       { 2, "[Python]" },
+                       { 3, "[PE-resources]" }
+                     };
 
 /**\struct ignore_node
  */
@@ -21,6 +24,8 @@ struct ignore_node {
        char       *value;
      };
 
+/** A dynamic array of \c "ignore_node".
+ */
 static smartlist_t *ignore_list;
 
 /**
@@ -38,45 +43,39 @@ static void cfg_parse (smartlist_t *sl, char *line)
   char   ignore [256], *p;
   struct ignore_node *node = NULL;
   BOOL   add_it = FALSE;
-  static const char  *section;
+  static const char *section = NULL;
 
   strip_nl (line);
   p = str_trim (line);
 
-  if (!stricmp(p,"[Registry]"))
-  {
-    section = sections[0];
-    return;
-  }
-  if (!stricmp(p,"[Python]"))
-  {
-    section = sections[1];
-    return;
-  }
-  if (!stricmp(p,"[PE-resources]"))
-  {
-    section = sections[2];
-    return;
-  }
   if (*p == '[')
   {
-    WARN ("Ignoring unknown section: %s.\n", p);
+    unsigned idx = list_lookup_value (p, sections, DIM(sections));
+
+    if (idx == UINT_MAX)
+         WARN ("Ignoring unknown section: %s.\n", p);
+    else section = sections[idx].name; /* remember the section for nex line */
     return;
   }
+
+  if (!section)
+     return;
 
   ignore[0] = '\0';
 
-  if (section && sscanf(p, "ignore = %256s", ignore) == 1 && ignore[0] != '\"')
-    add_it = TRUE;
+  if (sscanf(p, "ignore = %256s", ignore) == 1 && ignore[0] != '\"')
+     add_it = TRUE;
 
-  /* For stuff to "ignore with spaces", use this quite complex
-   * sscanf() expression.
-   *
-   * Ref: https://msdn.microsoft.com/en-us/library/xdb9w69d.aspx
+  /**
+   * For things to "ignore with spaces", the above \c sscanf() returned with
+   * \c "ignore[0] == '\"'" and \b not the full ignore string-value.
+   * Therefore use this quite complex \c sscanf() expression to get the whole
+   * string-value.
+   *   Ref: https://msdn.microsoft.com/en-us/library/xdb9w69d.aspx
    */
   if (ignore[0] == '\"' &&
-     sscanf(p, "ignore = \"%256[-_ :()\\/.a-zA-Z0-9^\"]\"", ignore) == 1)
-    add_it = TRUE;
+      sscanf(p, "ignore = \"%256[-_ :()\\/.a-zA-Z0-9^\"]\"", ignore) == 1)
+     add_it = TRUE;
 
   if (add_it)
   {
@@ -84,7 +83,7 @@ static void cfg_parse (smartlist_t *sl, char *line)
     node->section = section;
     node->value   = STRDUP (ignore);
     smartlist_add (sl, node);
-    DEBUGF (3, "[%s]: '%s'.\n", section, ignore);
+    DEBUGF (3, "%s: '%s'.\n", section, ignore);
   }
 }
 
@@ -102,35 +101,125 @@ int cfg_ignore_init (const char *fname)
     ignore_list = smartlist_read_file (file, (smartlist_parse_func)cfg_parse);
     FREE (file);
   }
+  cfg_ignore_dump();
   return (ignore_list != NULL);
 }
 
 /**
  * Lookup a \c value to test for ignore. Compare the \c section too.
  *
- * \param[in] section  look for the \c value in this section.
- * \param[in] value    the string-value to check.
+ * \param[in] section  Look for the \c value in this section.
+ * \param[in] value    The string-value to check.
  *
  * \retval 0 the \c section and \c value was not something to ignore.
  * \retval 1 the \c section and \c value was found in the \c ignore_list.
  */
 int cfg_ignore_lookup (const char *section, const char *value)
 {
-  int i, max = ignore_list ? smartlist_len (ignore_list) : 0;
+  int i, max;
 
+  if (section[0] != '[' || !ignore_list)
+     return (0);
+
+  max = smartlist_len (ignore_list);
   for (i = 0; i < max; i++)
   {
     const struct ignore_node *node = smartlist_get (ignore_list, i);
 
-    if (section && stricmp(section, node->section))
-       continue;
-    if (!stricmp(value, node->value))
+    if (!stricmp(section, node->section) && !stricmp(value, node->value))
     {
-      DEBUGF (3, "Found '%s' in [%s].\n", value, section);
+      DEBUGF (3, "Found '%s' in %s.\n", value, section);
       return (1);
     }
   }
   return (0);
+}
+
+/**
+ * Helper indices for \c cfg_ignore_first() and
+ * \c cfg_ignore_next().
+ */
+static int  next_idx = -1;
+static UINT curr_sec = UINT_MAX;
+
+/**
+ * Lookup the first ignored \c value in a \c section.
+ */
+const char *cfg_ignore_first (const char *section)
+{
+  const struct ignore_node *node;
+  int   i, max;
+  UINT  idx = list_lookup_value (section, sections, DIM(sections));
+
+  if (idx == UINT_MAX)
+  {
+    DEBUGF (2, "No such section: %s.\n", section);
+    goto not_found;
+  }
+
+  max = ignore_list ? smartlist_len (ignore_list) : 0;
+  for (i = 0; i < max; i++)
+  {
+    node = smartlist_get (ignore_list, i);
+    if (!stricmp(section, node->section))
+    {
+      next_idx = i + 1;
+      curr_sec = idx;
+      return (node->value);
+    }
+  }
+
+not_found:
+  next_idx = -1;
+  curr_sec = UINT_MAX;
+  return (NULL);
+}
+
+/**
+ * Lookup the next ignored \c value.
+ */
+const char *cfg_ignore_next (const char *section)
+{
+  const struct ignore_node *node;
+  int   i, max = smartlist_len (ignore_list);
+
+  /* cfg_ignore_first() not called or no ignorables found
+   * by cfg_ignore_first().
+   */
+  if (next_idx == -1 || curr_sec == UINT_MAX)
+     return (NULL);
+
+  for (i = next_idx; i < max; i++)
+  {
+    node = smartlist_get (ignore_list, i);
+    if (!stricmp(sections[curr_sec].name, section) &&
+        !stricmp(section, node->section))
+    {
+      next_idx = i + 1;
+      return (node->value);
+    }
+  }
+  next_idx = -1;
+  return (NULL);
+}
+
+/**
+ * Dump all ignored values in all sections.
+ */
+void cfg_ignore_dump (void)
+{
+  int i, j;
+
+  for (i = 0; i < DIM(sections); i++)
+  {
+    const char *ignored, *section = sections[i].name;
+
+    for (j = 0, ignored = cfg_ignore_first(section);
+         ignored;
+         ignored = cfg_ignore_next(section), j++)
+        ;
+        DEBUGF (2, "section: %-15s: num: %d.\n", section, j);
+  }
 }
 
 /**
@@ -149,7 +238,7 @@ void cfg_ignore_exit (void)
   {
     struct ignore_node *node = smartlist_get (ignore_list, i);
 
-    DEBUGF (3, "%d: ignore: '%s'\n", i, node->value);
+    DEBUGF (2, "%d: ignore: '%s'\n", i, node->value);
     FREE (node->value);
     FREE (node);
   }
--- src/ignore.h
+++ src/ignore.h
@@ -3,8 +3,11 @@
 #ifndef _IGNORE_H
 #define _IGNORE_H
 
-extern int  cfg_ignore_init (const char *fname);
-extern void cfg_ignore_exit (void);
-extern int  cfg_ignore_lookup (const char *section, const char *value);
+extern int         cfg_ignore_init (const char *fname);
+extern void        cfg_ignore_exit (void);
+extern int         cfg_ignore_lookup (const char *section, const char *value);
+extern const char *cfg_ignore_first (const char *section);
+extern const char *cfg_ignore_next (const char *section);
+extern void        cfg_ignore_dump (void);
 
 #endif /* _IGNORE_H */
