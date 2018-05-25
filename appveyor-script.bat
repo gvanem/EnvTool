a1bce2fd097868b0ec8790eb94c9a7acbe45c4ea
gvanem <gvanem@yahoo.no>
2018-05-25 12:40:08 +0000

    *) Create '.netrc' and '.authinfo' for AppVeyor test.    (create them in the '%APPVEYOR_BUILD_FOLDER%' folder).
    *) Use 'goto :EOF' to exit from sub-routine 'create_auth_files'
      or from the .bat-file.
    
    *) If building locally, use 'set APPVEYOR_BUILD_FOLDER=%CD%'
       for the full path.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -11,12 +11,12 @@ setlocal
 :: Then the env-var '%APPVEYOR_BUILD_FOLDER%' will not exist.
 ::
 :: Otherwise:
-::   Since only AppVeyor's Python2.7 is in the PATH by default.
-::   Add a Python 3.4 (x86) to the PATH.
+::   Since only Python2.7 is in AppVeyor's PATH by default, add this
+::   Python 3.4 (x86) to the PATH.
 ::
 ::
 if "%APPVEYOR_BUILD_FOLDER%" == "" (
-   set APPVEYOR_BUILD_FOLDER=.
+  set APPVEYOR_BUILD_FOLDER=%CD%
 ) else (
   set PATH=%PATH%;c:\Python34
   rem echo "%PATH%"
@@ -26,19 +26,24 @@ if %1. == build. goto build
 if %1. == test.  goto test
 
 echo Usage: %0 "build / test"
-exit /b 0
+goto :EOF
 
 :build
   cd %APPVEYOR_BUILD_FOLDER%\src
   msbuild -nologo -p:Configuration=Release -p:Platform="Win32" envtool.sln
-  exit /b 0
+  goto :EOF
 
 ::
 :: Run some "envtool" tests.
 ::
 :test
-  cd %APPVEYOR_BUILD_FOLDER%\src
   set COLUMNS=120
+  set APPDATA=%APPVEYOR_BUILD_FOLDER%
+
+  call :create_auth_files
+
+  cd %APPVEYOR_BUILD_FOLDER%\src
+
   echo Testing version output
   envtool -VVV
 
@@ -52,11 +57,36 @@ exit /b 0
   envtool --test --python=py3
 
   echo Testing ETP-searches (should fail)
-  envtool --test --evry:ftp.github.com
+  envtool -d --test --evry:ftp.github.com:21
 
   echo Testing check output
   envtool --check
 
   :: echo Testing win_glob
-  ::win_glob -fr "c:\Program Files (x86)\CMake"
+  :: win_glob -fr "c:\Program Files (x86)\CMake"
+
+  goto :EOF
+
+::
+:: Create a '.netrc' and '.authinfo' files for testing of 'src/auth.c' functions
+::
+:create_auth_files
+  echo Creating '%APPDATA%/.netrc'.
+
+  echo #                                                                     > .netrc
+  echo # This .netrc file was generated from "appveyor-script.bat".         >> .netrc
+  echo #                                                                    >> .netrc
+  echo machine host1  login user1     password password1                    >> .netrc
+  echo machine host2  login user2     password password2                    >> .netrc
+  echo default        login anonymous password your@email.address           >> .netrc
+
+  echo Creating '%APPDATA%/.authinfo'.
+  echo #                                                                     > .authinfo
+  echo # This .authinfo file was generated from "appveyor-script.bat".      >> .authinfo
+  echo #                                                                    >> .authinfo
+  echo machine host1  port 8080 login user1     password password1          >> .authinfo
+  echo machine host2  port 7070 login user2     password password2          >> .authinfo
+  echo default        port 21   login anonymous password your@email.address >> .authinfo
+
+  goto :EOF
 
