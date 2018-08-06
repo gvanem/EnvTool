dde762997a83deef95dc3e027b92f9654436090b
gvanem <gvanem@yahoo.no>
2018-08-06 21:23:35 +0000

    Added 'sort.[ch]'; preliminary functions for sorting the results.

---
+++ src/sort.c
@@ -0,0 +1,83 @@
+/**
+ * Handling of sort options `-S` and `"--sort"`.
+ */
+#include "envtool.h"
+#include "sort.h"
+
+static const struct search_list short_sort_methods[] = {
+           { SORT_FILE_NAME,      "n" },
+           { SORT_FILE_EXTENSION, "e"  },
+           { SORT_FILE_DATETIME,  "d" },
+           { SORT_FILE_DATETIME,  "t" },
+           { SORT_FILE_SIZE,      "s" },
+         };
+
+static const struct search_list long_sort_methods[] = {
+           { SORT_FILE_NAME,      "name" },
+           { SORT_FILE_EXTENSION, "ext"  },
+           { SORT_FILE_DATETIME,  "date" },
+           { SORT_FILE_DATETIME,  "time" },
+           { SORT_FILE_SIZE,      "size" },
+         };
+
+static char methods [200];
+
+static const char *get_methods (const struct search_list *l, size_t num)
+{
+  char  *p = methods;
+  size_t i, sz, left = sizeof(methods);
+
+  *p = '\0';
+  for (i = 0; i < num && left > 2; i++, l++)
+  {
+    sz = strlen (l->name);
+    _strlcpy (p, l->name, left);
+    p += sz;
+    *p = ',';
+    if (i == num - 1)
+       break;
+    p++;
+    left -= sz + 1;
+  }
+  *p = '\0';
+  return (methods);
+}
+
+const char *get_sort_methods_short (void)
+{
+  return get_methods (short_sort_methods, DIM(short_sort_methods));
+}
+
+const char *get_sort_methods_long (void)
+{
+  return get_methods (long_sort_methods, DIM(long_sort_methods));
+}
+
+/**
+ * Set `opt.sort_method` based on `short_opt` or `long_opt`.
+ */
+BOOL set_sort_method (const char *short_opt, const char *long_opt)
+{
+  SortMethod m = SORT_FILE_UNSORTED;
+
+  ASSERT (short_opt || long_opt);
+
+  if (short_opt)
+  {
+    DEBUGF (0, "got \"-S\" option: '%s'.\n", short_opt);
+    m = list_lookup_value (short_opt, short_sort_methods, DIM(short_sort_methods));
+    if (m == UINT_MAX)
+       return (FALSE);
+  }
+  else
+  {
+    DEBUGF (0, "got \"--sort\" option: '%s'.\n", long_opt);
+    m = list_lookup_value (long_opt, long_sort_methods, DIM(long_sort_methods));
+    if (m == UINT_MAX)
+       return (FALSE);
+  }
+  opt.sort_method = m;
+  return (TRUE);
+}
+
+
---
+++ src/sort.h
@@ -0,0 +1,22 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+/** \enum SortMethod
+ * Used with the "-S" or "--sort" cmd-line options to sort
+ * matches on file-name, file-extension, date/time or size.
+ * Always sort from low to high.
+ */
+typedef enum SortMethod {
+        SORT_FILE_UNSORTED,
+        SORT_FILE_NAME,
+        SORT_FILE_EXTENSION,
+        SORT_FILE_DATETIME,
+        SORT_FILE_SIZE
+      } SortMethod;
+
+extern BOOL        set_sort_method (const char *short_opt, const char *long_opt);
+extern const char *get_sort_methods_short (void);
+extern const char *get_sort_methods_long (void);
+
+#endif
+
