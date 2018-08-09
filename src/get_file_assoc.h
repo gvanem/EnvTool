097e9865237547d7abf16292d48d64656ae57ffe
gvanem <gvanem@yahoo.no>
2018-08-09 06:09:06 +0000

    New file: finds the associated handler for a file-extension. Not used yet.

---
+++ src/get_file_assoc.h
@@ -0,0 +1,11 @@
+/** \file get_file_assoc.h
+ *  \ingroup Misc
+ */
+#ifndef _GET_FILE_ASSOC_H
+#define _GET_FILE_ASSOC_H
+
+extern BOOL get_file_assoc_all (const char *extension);
+extern BOOL get_file_assoc (const char *extension, char **program, char **exe);
+
+#endif
+
