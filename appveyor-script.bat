57864a1bac15e1a0a92d452508cef195d8cd376a
gvanem <gvanem@yahoo.no>
2018-09-27 11:16:50 +0000

    *) Added blank lines before "Testing ...". *) If running locally, set 'APPVEYOR_BUILD_FOLDER' to the directory of    the 'appveyor-script.bat' file; '%~dp0'. *) Do similarily for "Usage: ...".

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -10,19 +10,19 @@ setlocal
 :: One could also run this .BAT-file locally before testing with AppVeyor.
 :: Then the env-var '%APPVEYOR_BUILD_FOLDER%' will not exist.
 ::
-:: Otherwise:
+:: Otherwise when running on AppVeyor:
 ::   Since only Python2.7 is in AppVeyor's PATH by default, add this
 ::   Python 3.4 (x86) to the PATH.
 ::
 ::
-if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%CD%
+if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
 
 set PATH=%PATH%;c:\Python34
 
 if %1. == build. goto build
 if %1. == test.  goto test
 
-echo Usage: %0 "build / test"
+echo Usage: %~dp0appveyor-script.bat "build / test"
 goto :EOF
 
 :build
@@ -37,41 +37,46 @@ goto :EOF
   set COLUMNS=120
   set APPDATA=%APPVEYOR_BUILD_FOLDER%
 
+  cd %APPDATA%
   call :create_auth_files
 
-  cd %APPVEYOR_BUILD_FOLDER%\src
+  cd %APPDATA%\src
 
   echo Testing version output
   envtool -VVV
 
+  echo.
   echo Testing test output (show owner in test_PE_wintrust())
   envtool --test --owner
 
+  echo.
   echo Testing Python2 test output
   envtool --test --python=py2
 
+  echo.
   echo Testing Python3 test output
   envtool --test --python=py3
 
+  echo.
   echo Testing VCPKG output
   envtool --vcpkg azure-u*
 
+  echo.
   echo Testing ETP-searches (should fail)
   envtool -d --test --evry:ftp.github.com:21
 
+  echo.
   echo Testing check output
   envtool --check
 
   :: echo Testing win_glob
   :: win_glob -fr "c:\Program Files (x86)\CMake"
 
-  cd %APPVEYOR_BUILD_FOLDER%
-  del /q .netrc .authinfo
-
+  del /q %APPDATA%\.netrc %APPDATA%\.authinfo
   goto :EOF
 
 ::
-:: Create a '.netrc' and '.authinfo' files for testing of 'src/auth.c' functions
+:: Create a '<root>\.netrc' and '<root>\.authinfo' files for testing of 'src/auth.c' functions
 ::
 :create_auth_files
   echo Creating '%APPDATA%/.netrc'.
@@ -90,6 +95,6 @@ goto :EOF
   echo machine host1  port 8080 login user1     password password1          >> .authinfo
   echo machine host2  port 7070 login user2     password password2          >> .authinfo
   echo default        port 21   login anonymous password your@email.address >> .authinfo
-
+  echo.
   goto :EOF
 
