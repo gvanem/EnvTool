c0e9a8ac5544e394ff9a117b8e30b2ad80a48a5d
gvanem <gvanem@yahoo.no>
2019-11-09 17:51:56 +0000

    Added functions for getting at 4NT/TCC file-descriptions; from a hidden 'DESCRIPT.ION' file. These functions are usable from e.g. a 'CMD' shell too.

---
+++ src/description.h
@@ -0,0 +1,12 @@
+/** \file description.h
+ *  \ingroup Misc
+ */
+#ifndef _DESCRIPTION_H
+#define _DESCRIPTION_H
+
+extern void        file_descr_init (void);
+extern void        file_descr_exit (void);
+extern const char *file_descr_get (const char *file);
+
+#endif /* _DESCRIPTION_H */
+
