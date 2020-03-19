46d22f07bfbcb075d539b7874c8537d21f8f64e3
gvanem <gvanem@yahoo.no>
2020-03-19 17:12:22 +0000

    No such function 'cfg_ignore_init()'.

--- src/ignore.h
+++ src/ignore.h
@@ -4,7 +4,6 @@
 #ifndef _IGNORE_H
 #define _IGNORE_H
 
-extern int         cfg_ignore_init (const char *fname);
 extern void        cfg_ignore_exit (void);
 extern int         cfg_ignore_lookup (const char *section, const char *value);
 extern const char *cfg_ignore_first (const char *section);
